f747dbbae85ed14d9279e4fa7a2da26a5568f929
turi4200 <konarthur@gmail.com>
2021-01-06 14:15:00 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,9 +43,10 @@ EOF
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-sh -c "yarn" \
-&& sh -c "cp .env.prod .env" \
-&& sh -c "${NODE_ENV_PREPEND} yarn build" \
+sh -c "cp .env.prod .env" \
+&& sh -c "cd app-web" \
+&& sh -c "yarn build" \
+&& sh -c "cd .." \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
               --no-progress \
