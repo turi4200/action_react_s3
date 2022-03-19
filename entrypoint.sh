353df2c77f35781357585832e7f2f2c11e65d077
turi4200 <konarthur@gmail.com>
2022-03-19 17:21:21 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,7 +43,8 @@ EOF
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-sh -c "cp .env.${ENVTYPE} web/.env" \
+sh -c "yarn set version latest" \
+&& sh -c "cp .env.${ENVTYPE} web/.env" \
 && sh -c "cd web && yarn && yarn build" \
 && sh -c "aws s3 rm s3://${AWS_S3_BUCKET} --recursive" \
 && sh -c "aws cloudfront create-invalidation \
